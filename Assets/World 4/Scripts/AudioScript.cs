6f0c968ffdd9bc09cc4aa613403bcc6ac7f92ff9
Unknown <viktorija.kondratjevaite@gmail.com>
2019-11-06 17:22:41 +0200

    Added background audio

---
+++ Assets/World 4/Scripts/AudioScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+
+    private bool paused = false;
+
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+        MusicSource.Play();
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(5, 5, 100, 50), "Mute music"))
+        {
+            if (paused)
+            {
+                MusicSource.UnPause();
+                paused = false;
+            }
+            else
+            {
+                MusicSource.Pause();
+                paused = true;
+            }
+        }
+    }
+}
