972fef334e1b9bcafa4f17df767b28085ee0efaf
Nerius Zarnauskas <nerius.zarnauskas@vinted.com>
2019-11-07 19:04:02 +0200

    Change mobs movement

--- Assets/World 4/Scripts/WalkThenRunScript.cs
+++ Assets/World 4/Scripts/WalkThenRunScript.cs
@@ -8,20 +8,24 @@ public class WalkThenRunScript : MonoBehaviour
     public float RunSpeed;
     public Vector3 Direction;
     public TimeInterval timeIntervalToRun;
+    private GameObject Player;
     private Rigidbody _rigidbody;
     private bool _run = false;
     private float _timeAfterToRun;
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        Player = GameObject.Find("Player");
         _timeAfterToRun = Time.time + UnityEngine.Random.Range(timeIntervalToRun.start, timeIntervalToRun.end);
     }
 
     void Update()
     {
-        if (Time.time > _timeAfterToRun)
+        if (_run == false && Time.time > _timeAfterToRun)
         {
             _run = true;
+            Direction = Player.transform.position - transform.position;
+            Direction = Direction / Direction.magnitude;
         }
     }
 
