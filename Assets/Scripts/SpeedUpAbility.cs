f4e3f259b966a7b2833e991a0b0cf4fda948db53
hjuknonis <h.juknonis@gmail.com>
2019-11-30 16:48:04 +0200

    Added ability script + speed up ability

---
+++ Assets/Scripts/SpeedUpAbility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Abilities/SpeedUpAbility")]
+public class SpeedUpAbility : AbilityScript
+{
+    public float movementSpeed, defaultSpeed;
+    private PlayerMovementControlScript player; 
+
+    public override void Initialize(GameObject obj)
+    {
+        player = obj.GetComponent<PlayerMovementControlScript>();
+        defaultSpeed = player.Speed;
+    }
+
+    public override void AbilityStart()
+    {
+        player.Speed = movementSpeed;
+    }
+    
+    public override void AbilityEnd()
+    {
+        player.Speed = defaultSpeed;
+    }
+}
